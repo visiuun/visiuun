42c743cd0bfdfb4853d69b5a3be57561bdaca3c5
V1SIONUSE <118626319+V1SIONUSE@users.noreply.github.com>
2024-12-19 13:26:46 +0100

    Add files via upload

---
+++ assets/js/projects.js
@@ -0,0 +1,52 @@
+const projects = [
+    {
+        "name": "Nekobox",
+        "description": "Free File Hosting And Sharing Platform.",
+        "url": "/Nekobox/upload"
+    },
+    {
+        "name": "Neko",
+        "description": "A Very Basic Chatbot That Almost Never Works!",
+        "url": "/chatbotv2/neko"
+    },
+    {
+        "name": "APIs",
+        "description": "Documentation And Information On My API Endpoints",
+        "url": "/home/apis"
+    },
+    {
+        "name": "Rock Paper Scissors Dice",
+        "description": "A Rock Paper Scissors Dice Game.",
+        "url": "/tools/rps/rps"
+    },
+    {
+        "name": "Chatbot",
+        "description": "An online chatbot ui based on brainshop.ai api",
+        "url": "https://popcat.xyz/chat"
+    },
+    {
+        "name": "Cloe",
+        "description": "A bot website created for a commission, it is also one of the best websites i've ever created",
+        "url": "https://cloe.gg"
+    },
+    {
+        "name": "YouTube Downloader",
+        "description": "A simple and easy to use YouTube Video Downloader tool!",
+        "url": "https://popcat.xyz/youtube-downloader"
+    },
+    {
+        "name": "TODO List",
+        "description": "An Online Todo List with localStorage option to save your tasks on one device only and a Login system to save them globally!",
+        "url": "https://popcat.xyz/todo"
+    },
+    {
+        "name": "Code Bin",
+        "description": "Store and Access your codes on the cloud",
+        "url": "https://code.popcat.xyz"
+    },
+    {
+        "name": "URL Shortener",
+        "description": "A simple, easy to use URL shortener with statistics on the number of clicks on your endpoint",
+        "url": "https://url.popcat.xyz"
+    },
+]
